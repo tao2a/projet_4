d1b0f36397e6656ce99f159441ade6949d23f795
Tao2a <auccia@free.fr>
2017-11-21 13:56:08 +0100

    Generating Entity

---
+++ src/AppBundle/Entity/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: auccia
+ * Date: 21/11/2017
+ * Time: 12:44
+ */
+
+namespace AppBundle\Entity;
+
+
+class Customer
+{
+
+}
---
+++ src/AppBundle/Entity/Visit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: auccia
+ * Date: 21/11/2017
+ * Time: 12:49
+ */
+
+namespace AppBundle\Entity;
+
+
+class Visit
+{
+
+}
